f0f1ed3793dc710239831457b2e82d49aa1aebc2
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2026-07-08 12:41:10 +0300

    configure vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/customs-kit/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        calc: resolve(__dirname, 'calc.html'),
+        currency: resolve(__dirname, 'currency.html'),
+        base64: resolve(__dirname, 'base64-converter.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
+  },
 });
